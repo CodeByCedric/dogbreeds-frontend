0f1a92998689c40313728734f4321a1b29e85f4b
cedric.puystjens <cedric.puystjens@student.howest.be>
2023-08-08 19:37:18 +0200

    Moved filter to separate component

---
+++ src/components/DogFilter.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="filter">
+    <select name="exercise" id="exercise">
+      <option disabled value="placeholder" selected>Exercise Needs</option>
+      <option value="all">All</option>
+      <option value="very high">Perpetual, never ending exercise</option>
+      <option value="high">High</option>
+      <option value="moderate">Moderate</option>
+      <option value="low">No exercise for me or my dog</option>
+    </select>
+    <select name="trainability" id="trainability">
+      <option disabled value="placeholder" selected>Trainability</option>
+      <option value="all">All</option>
+      <option value="very high">I will learn everything</option>
+      <option value="high">High</option>
+      <option value="moderate">Moderate</option>
+      <option value="low">Low</option>
+      <option value="very-low">You will teach me nothing</option>
+    </select>
+
+    <select name="grooming" id="grooming">
+      <option disabled value="placeholder" selected>Grooming Requirements</option>
+      <option value="all">All</option>
+      <option value="very high">Primped and Pampered Pooches</option>
+      <option value="high">High-Maintenance Hairdos</option>
+      <option value="moderate">Routine Rinsers</option>
+      <option value="low">Basic Brushers</option>
+      <option value="very-low">No-Fuss Natural</option>
+    </select>
+  </div>
+</template>
+
+<style scoped>
+#filter {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+}
+
+#filter select {
+  margin: 0 1rem;
+  padding: 0.25rem;
+  border-radius: 0.25rem;
+  border: 1px solid #39495c;
+  font-size: 1rem;
+  width: 15rem;
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import { ref, onMounted } from 'vue';
 import DogService from '../services/DogService.js';
 import DogCard from '../components/DogCard.vue';
+import DogFilter from '../components/DogFilter.vue';
 
 const dogs = ref(null);
 
@@ -22,35 +23,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <header id="filter">
-    <!-- <select name="exercise" id="exercise">
-      <option disabled value="placeholder" selected>Exercise Needs</option>
-      <option value="all">All</option>
-      <option value="very high">Perpetual, never ending exercise</option>
-      <option value="high">High</option>
-      <option value="moderate">Moderate</option>
-      <option value="low">No exercise for me or my dog</option>
-    </select>
-    <select name="trainability" id="trainability">
-      <option disabled value="placeholder" selected>Trainability</option>
-      <option value="all">All</option>
-      <option value="very high">I will learn everything</option>
-      <option value="high">High</option>
-      <option value="moderate">Moderate</option>
-      <option value="low">Low</option>
-      <option value="very-low">You will teach me nothing</option>
-    </select>
-
-    <select name="grooming" id="grooming">
-      <option disabled value="placeholder" selected>Grooming Requirements</option>
-      <option value="all">All</option>
-      <option value="very high">Primped and Pampered Pooches</option>
-      <option value="high">High-Maintenance Hairdos</option>
-      <option value="moderate">Routine Rinsers</option>
-      <option value="low">Basic Brushers</option>
-      <option value="very-low">No-Fuss Natural</option>
-    </select> -->
-  </header>
+  <DogFilter></DogFilter>
   <main>
     <div class="dogs">
       <DogCard v-for="dog in dogs" :key="dog.id" :dog="dog"></DogCard>
@@ -63,21 +36,5 @@ onMounted(() => {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-}
-
-#filter {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 1rem;
-}
-
-#filter select {
-  margin: 0 1rem;
-  padding: 0.25rem;
-  border-radius: 0.25rem;
-  border: 1px solid #39495c;
-  font-size: 1rem;
-  width: 15rem;
 }
 </style>
