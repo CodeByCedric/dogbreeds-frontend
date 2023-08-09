4e399d26a73e59ef88d83cc25ba6a25710e74b6d
cedric.puystjens <cedric.puystjens@student.howest.be>
2023-08-09 14:19:30 +0200

    Setup i18n

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
+import { I18n } from './i18n';
 
 
 init();
@@ -25,5 +26,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(I18n);
 
 app.mount('#app');
