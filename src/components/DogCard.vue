2966f7cfa84d285fd6978e06b8b838c005c3e054
cedric.puystjens <cedric.puystjens@student.howest.be>
2023-08-10 18:09:51 +0200

    Consolidate Home and Dashboard card components
    Use conditionals instead of different components to avoid duplicate code

--- src/components/DogCard.vue
+++ src/components/DogCard.vue
@@ -1,15 +1,17 @@
 <template>
-  <RouterLink class="dog-card-link" :to="{ name: 'dog-details', params: { id: dog.id } }">
-    <div class="dog-card">
-      <h2>{{ dog.name }}</h2>
-      <ul>
-        <li>{{ $t('exercise_needs.title') }}: {{ dog.exercise_needs }}</li>
-        <li>{{ $t('trainability.title') }}: {{ dog.trainability }}</li>
-        <li>{{ $t('grooming_requirements.title') }}: {{ dog.grooming_requirements }}</li>
-      </ul>
-      <p v-html="truncateDescription(dog.description, 100)"></p>
+  <div class="dog-card" @click="navigateToDogDetails">
+    <h2>{{ dog.name }}</h2>
+    <ul>
+      <li>{{ $t('exercise_needs.title') }}: {{ dog.exercise_needs }}</li>
+      <li>{{ $t('trainability.title') }}: {{ dog.trainability }}</li>
+      <li>{{ $t('grooming_requirements.title') }}: {{ dog.grooming_requirements }}</li>
+    </ul>
+    <p v-html="truncateDescription(dog.description, 100)"></p>
+    <div class="icon-container" v-if="$route.path === '/dashboard'">
+      <img class="icons" src="../assets/icons/edit.svg" alt="edit icon" @click.stop="navigateToEditDog"/>
+      <img class="icons" src="../assets/icons/delete.svg" alt="delete icon" @click.stop="deleteDog"/>
     </div>
-  </RouterLink>
+  </div>
 </template>
 
 <script>
@@ -20,9 +22,28 @@ export default {
       required: true,
     },
   },
+  computed: {
+    isDashboard() {
+      return this.$route.path === '/dashboard';
+    },
+  },
   methods: {
     truncateDescription(text, length) {
       return text.length > length ? text.substring(0, length) + '...' : text;
+    },
+    navigateToDogDetails() {
+      console.log(this.$route.path);
+      if (this.isDashboard) return;
+      else {
+        this.$router.push({ name: 'dog-details', params: { id: this.dog.id } });
+      }
+      
+    },
+    navigateToEditDog() {
+      this.$router.push({ name: 'edit-dog', params: { id: this.dog.id } });
+    },
+    deleteDog() {
+      // delete the dog
     },
   },
 };
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -2,20 +2,20 @@
   <DogFilter></DogFilter>
   <div class="dogs">
     <RouterLink :to="{ name: 'add-dog' }"><p class="dog-card" id="create-dog">+</p></RouterLink>
-    <DashboardDogCard v-for="dog in dogs" :key="dog.id" :dog="dog"></DashboardDogCard>
+    <DogCard v-for="dog in dogs" :key="dog.id" :dog="dog"></DogCard>
   </div>
 </template>
 
 <script>
 import DogService from '../services/DogService.js';
 import DogFilter from '../components/DogFilter.vue';
-import DashboardDogCard from '../components/DashboardDogCard.vue';
+import DogCard from '../components/DogCard.vue';
 
 export default {
   emits: ['login'],
   components: {
+    DogCard,
     DogFilter,
-    DashboardDogCard,
   },
   watch: {
     '$i18n.locale': function (newLocale) {
@@ -60,19 +60,9 @@ export default {
 
 <style scoped>
 #create-dog {
-  display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
   font-size: 5rem;
-  padding: 1.25rem;
-  width: 15.5rem;
-  height: 17.5rem;
-  margin: 1rem;
-  cursor: pointer;
-  border: 1px solid #39495c;
-  margin-bottom: 1rem;
-  text-decoration: none;
 }
 
 #create-dog:hover {
