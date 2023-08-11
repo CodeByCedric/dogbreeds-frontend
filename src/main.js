638234927090299d9e276b69b2ce4041aa0e2caa
cedric.puystjens <cedric.puystjens@student.howest.be>
2023-08-11 13:36:56 +0200

    Remove cache attempt from PWA

--- src/main.js
+++ src/main.js
@@ -5,23 +5,6 @@ import router from './router';
 import i18n from './locales/i18n';
 import './assets/css/global.css';
 
-init();
-
-function init() {
-  registerServiceWorker();
-}
-
-function registerServiceWorker() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-      .register('src/sw/ServiceWorker.js')
-      .then((res) => console.log('Succesfully registered serviceworker with scope', res.scope))
-      .catch((err) => console.log('Error registering serviceworker'));
-  } else {
-    console.log('Serviceworker not supported');
-  }
-}
-
 const app = createApp(App);
 
 app.use(createPinia());
